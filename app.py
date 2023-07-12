b6c3286747541e800e43a63fc0b29f97f327bac7
pcalebho <pcalebho@gmail.com>
2023-07-11 22:44:12 -0500

    added more forms

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 import streamlit as st
-import datetime
+from datetime import datetime
 from pymongo import MongoClient
 from moviepy.editor import VideoFileClip
 
-@st.experimental_singleton()
+@st.cache_resource()
 def init_connection():
     return MongoClient("mongodb+srv://st.secrets.db_username:st.secrets.\
         db_pswd@st.secrets.cluster_name.5ygnbeg.mongodb.net/?retryWrites=true&w=majority")
 
-st.button('Start Session')
+client = init_connection()
+db = client.missions
+mission_collection = db.test_mission_collection
 
-#Entry addition
-st.button('Add Entry')
-
-
-with st.form(key = 'log_entry'):
-    st.header('Log Fault')
-    fault_name = st.text_input('Fault Name')
-    password = st.text_input('Description of fault and what led up to it.')
+with st.form(key = 'mission_set'):
+    st.header('Start Session')
+    st.markdown('Type in mission name/description. Should be done in snake case, and cannot\
+            start with a number. It will be used as a collection name.')
+    mission_name = st.text_input('Mission Name/Description')
     st.form_submit_button('Add')
 
+#Entry addition
+if st.button('Add Entry',key='add_fault_button'):
+    fault_time = datetime.now()
+    with st.form(key = 'log_entry'):
+        st.header(mission_name)
+        header = 'Fault @ ' + fault_time.strftime('%m/%d/%y - %H:%M:%S')
+        st.subheader(header)
+        fault_name = st.text_input('Fault Name')
+        password = st.text_input('Description of fault and what led up to it.')
+        st.form_submit_button('Add')
 
+if st.button('Create clips'):
+    pass

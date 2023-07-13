54af23338fa418f44ec4de5cae1c8a3446ab9c93
pcalebho <pcalebho@gmail.com>
2023-07-12 23:46:00 -0500

    working only when the button is touched?

--- Add_logs.py
+++ Add_logs.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pymongo import MongoClient
 from moviepy.editor import VideoFileClip
 from config import fault_options
+from streamlit import session_state as ss
 
 @st.cache_resource()
 def init_connection():
@@ -24,6 +25,8 @@ if 'fault_type' not in st.session_state:
 if 'fault_description' not in st.session_state:
     st.session_state.fault_description = ' '
 
+if 'btn_time' not in ss:
+    ss.btn_time = ' '
 
 def write_to_db(collection, time, type = None, description = None):
     if type is None and description is None:
@@ -46,10 +49,10 @@ st.markdown(css, unsafe_allow_html=True)
 with mission_container:
     st.subheader('1. Set Mission Description')
     with st.form(key = 'mission_set'):
-        st.radio('Robot unit',['3.3','4.1','4.2','4.3'])
+        st.radio('Robot unit',['3.3','4.1','4.2','4.3'], key ='unit')
         st.markdown('Type in mission name/description. Should be done in snake case, and cannot\
                 start with a number. It will be used as a collection name.')
-        mission_name = st.text_input('Mission Name/Description')
+        mission_name = st.text_input('Mission Name/Description', key = 'mission_name')
         st.form_submit_button('Set Mission')
 
 
@@ -61,21 +64,22 @@ with col1:
         disabled = True
 
     fault_time = ''
-
+    str_time = ''
+    ss.btn_time = '0:00'
     if st.button('Get Time'):
         raw_time = datetime.now()
-        fault_time = raw_time.strftime('%m/%d/%y - %H:%M:%S')
+        ss.btn_time = raw_time.strftime('%m/%d/%y - %H:%M:%S')
+
 
     with st.form(key = 'log_entry', clear_on_submit=True):
-        fault_time = st.text_input('Fault Time', value = fault_time)
-        fault_type = st.selectbox('Fault Type', fault_options)
+        fault_time = st.text_input('Fault Time', key = 'fault_time', value= ss.btn_time)
+        fault_type = st.selectbox('Fault Type', fault_options, key = 'fault_type')
         fault_description = st.text_input('Description of fault and what led up to it.', \
                                                     key = 'fault_description')
         submit_btn = st.form_submit_button('Add', disabled=disabled) #, on_click= write_to_db(mission_name,\
                                                             #              fault_time))
         if submit_btn:
-            doc = {"fault_type": fault_type, "fault_description": fault_description, \
-                    "fault_time": fault_time}
+            doc = {"fault_type": fault_type, "fault_description": fault_description, "fault_time": fault_time}
             db[mission_name].insert_one(doc)
 
 with col2:
@@ -83,5 +87,5 @@ with col2:
 
 with upload_container:
     st.subheader('3. Upload Video')
-    st.file_uploader('')
+    st.file_uploader('Upload')
     st.button('Create clips')

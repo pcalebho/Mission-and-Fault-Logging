5794f0a0a6ea845462d9477225322be5ee7b7f91
pcalebho <pcalebho@gmail.com>
2023-07-13 21:24:01 -0500

    got program to correctly input data into db

--- Add_logs.py
+++ Add_logs.py
@@ -1,5 +1,5 @@
-import streamlit as st
-from datetime import datetime
+import streamlit as st  
+import datetime
 from pymongo import MongoClient
 from moviepy.editor import VideoFileClip
 from config import fault_options
@@ -12,6 +12,7 @@ def init_connection():
 client = init_connection()
 db = client.missions
 
+
 #Create containers
 mission_container = st.container()
 col1, col2 = st.columns(spec=[.6, .4])
@@ -20,13 +21,14 @@ upload_container = st.container()
 mission_name = ""
 
 if 'fault_type' not in st.session_state:
-    st.session_state.fault_type = ' '
+    st.session_state.fault_type = fault_options[0]
 
-if 'fault_description' not in st.session_state:
-    st.session_state.fault_description = ' '
+if 'date' not in ss:
+    ss.date = datetime.datetime.now().date()
 
-if 'btn_time' not in ss:
-    ss.btn_time = ' '
+if 'time' not in ss:
+    ss.time = datetime.datetime.now().time()
+
 
 def write_to_db(collection, time, type = None, description = None):
     if type is None and description is None:
@@ -46,6 +48,7 @@ css = r'''
 '''
 st.markdown(css, unsafe_allow_html=True)
 
+
 with mission_container:
     st.subheader('1. Set Mission Description')
     with st.form(key = 'mission_set'):
@@ -53,7 +56,9 @@ with mission_container:
         st.markdown('Type in mission name/description. Should be done in snake case, and cannot\
                 start with a number. It will be used as a collection name.')
         mission_name = st.text_input('Mission Name/Description', key = 'mission_name')
-        st.form_submit_button('Set Mission')
+        mission_set = st.form_submit_button('Set Mission')
+        if mission_set and mission_name != '':
+            st.success('Mission Set!')
 
 
 #Entry addition
@@ -63,23 +68,23 @@ with col1:
     if mission_name == "":
         disabled = True
 
-    fault_time = ''
+
     str_time = ''
-    ss.btn_time = '0:00'
-    if st.button('Get Time'):
-        raw_time = datetime.now()
-        ss.btn_time = raw_time.strftime('%m/%d/%y - %H:%M:%S')
+    if st.button('Get datetime', key = 'get_time_btn'):
+        raw_time = datetime.datetime.now()
+        ss.time = raw_time.time()
+        ss.date = raw_time.date()
 
-
+    
     with st.form(key = 'log_entry', clear_on_submit=True):
-        fault_time = st.text_input('Fault Time', key = 'fault_time', value= ss.btn_time)
-        fault_type = st.selectbox('Fault Type', fault_options, key = 'fault_type')
-        fault_description = st.text_input('Description of fault and what led up to it.', \
-                                                    key = 'fault_description')
-        submit_btn = st.form_submit_button('Add', disabled=disabled) #, on_click= write_to_db(mission_name,\
-                                                            #              fault_time))
-        if submit_btn:
-            doc = {"fault_type": fault_type, "fault_description": fault_description, "fault_time": fault_time}
+        date = st.date_input('Date', key = 'date', value = ss.date)
+        time = st.time_input('Fault Time', key = 'time', value= ss.time)
+        type = st.selectbox('Fault Type', options=fault_options, key = 'fault_type')
+        description = st.text_input('Description of fault and what led up to it.')
+        submitted = st.form_submit_button('Add', disabled=disabled) 
+
+        if submitted:
+            doc = {"fault_type": type, "fault_description": description, "fault_time": str(ss.time), "fault_date": str(ss.date)}
             db[mission_name].insert_one(doc)
 
 with col2:

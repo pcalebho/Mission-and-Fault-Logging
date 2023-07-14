7f6b0bd9c7d3472cf4806ced536b5292cc278b7e
pcalebho <pcalebho@gmail.com>
2023-07-13 23:13:09 -0500

    attempted to add preview table

--- Add_logs.py
+++ Add_logs.py
@@ -1,6 +1,8 @@
 import streamlit as st  
 import datetime
+import pandas as pd
 from pymongo import MongoClient
+from pymongo.errors import OperationFailure
 from moviepy.editor import VideoFileClip
 from config import fault_options
 from streamlit import session_state as ss
@@ -8,9 +10,33 @@ from streamlit import session_state as ss
 @st.cache_resource()
 def init_connection():
     return MongoClient("mongodb+srv://pcalebho:UISBvUYTesMft5AX@matcluster.5ygnbeg.mongodb.net/")
+    
 
 client = init_connection()
 db = client.missions
+
+def preview_db(collection):
+    try:
+        db.validate_collection(collection)  # Try to validate a collection
+    except OperationFailure:  # If the collection doesn't exist
+        return None
+
+    list = {
+        'Datetime': [],
+        'Fault Type': []
+    }
+    data = db[collection].find()
+
+
+    for entry in data:
+        list['Datetime'] += f"{entry['date']} {entry['time']}"
+        print('hello')
+        list['Fault Type'] += entry['type']
+
+    df = pd.DataFrame(list)
+    df = df.sort_values(by='Datetime',ascending=False)
+    return df
+        
 
 
 #Create containers
@@ -29,6 +55,9 @@ if 'date' not in ss:
 if 'time' not in ss:
     ss.time = datetime.datetime.now().time().strftime('%H:%M:%S')
 
+if 'mission' not in ss:
+    ss.mission = ''
+
 
 css = r'''
     <style>
@@ -45,6 +74,7 @@ with mission_container:
         st.markdown('Type in mission name/description. Should be done in snake case, and cannot\
                 start with a number. It will be used as a collection name.')
         mission_name = st.text_input('Mission Name/Description', key = 'mission_name')
+        ss.mission = mission_name
         mission_set = st.form_submit_button('Set Mission')
         if mission_set and mission_name != '':
             st.success('Mission Set!')
@@ -66,7 +96,7 @@ with col1:
 
     
     with st.form(key = 'log_entry', clear_on_submit=True):
-        exception = st.radio(label='Exception/Non-exception Fault', options=('Exception', 'Non-exception'))        
+        exception = st.radio(label='Exception/Non-exception Fault', options=('Non-exception','Exception'))        
         date = st.date_input('Date', key = 'date', value = ss.date)
         time = st.text_input('Time', key = 'time', value= ss.time, max_chars=8, help= 'format (HH\:MM\:SS)')
         type = st.selectbox('Fault Type', options=fault_options, key = 'fault_type')
@@ -74,11 +104,23 @@ with col1:
         submitted = st.form_submit_button('Add', disabled=disabled) 
 
         if submitted:
-            doc = {"exception": exception, "type": type, "description": description, "time": ss.time, "date": str(ss.date)}
+            doc = {
+                "exception": exception, 
+                "type": type, 
+                "description": description, 
+                "time": ss.time, 
+                "date": str(ss.date)
+            }
             db[mission_name].insert_one(doc)
 
 with col2:
-    pass 
+    st.subheader('Entries')
+    if ss.mission_name != '':
+        preview_table = preview_db(ss.mission)
+        if preview_table is not None:
+            st.table(preview_table)
+        else:
+            st.write('No entries found')
 
 with upload_container:
     st.subheader('3. Upload Video')

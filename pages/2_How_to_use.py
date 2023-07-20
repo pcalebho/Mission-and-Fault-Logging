ac128459e6834a94c030d42288ebcfafcfb1dd4f
pcalebho <pcalebho@gmail.com>
2023-07-19 20:29:26 -0500

    compress some large gifs

--- pages/2_How_to_use.py
+++ pages/2_How_to_use.py
@@ -1,3 +1,31 @@
 import streamlit as st
+import base64
+def display_local_gif(filename):
+    file_ = open(f"tutorials/{filename}", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" width = "600">',
+        unsafe_allow_html=True,
+    )
 
-st.subheader('Tutorial Video')
+st.title('How to Use')
+
+st.subheader('Create a new mission...')
+display_local_gif('Mission_set_1_optimized.gif')
+
+st.subheader('Or use an existing one')
+display_local_gif('Add_existing_mission_2_optimized.gif')
+
+st.subheader('Add faults to your mission')
+display_local_gif('Add_fault_3_optimized.gif')
+
+st.subheader('If you want to get the current time.')
+display_local_gif('Get_time_4.gif')
+
+st.subheader('Create clips of all your ')
+display_local_gif('Uploading_videos_5.gif')
+
+st.subheader('Review details of faults')
+display_local_gif('Review_faults_6_optimized.gif')
